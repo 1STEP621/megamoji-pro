fb61e54fe7b287a176dd735082fa4f8d3e192e13
1STEP621 <onestep621@gmail.com>
2023-08-11 11:32:39 +0900

    画像サイズ変更に対応

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", function () {
-  const inputEvent = new Event('input', { bubbles: true });
-  const changeEvent = new Event('change', { bubbles: true });
+  const inputEvent = (value) => new Event('input', { bubbles: false, value: value });
+  const changeEvent = (value) => new Event('change', { bubbles: true, target: { value: value } });
 
   const params = new URLSearchParams(location.search);
 
@@ -18,39 +18,49 @@ window.addEventListener("load", function () {
     await setValue('select[name="切り抜き"]', params.get('clip') ?? 0);
     if (params.get('transparent') != null) await sendClick('button[name="背景色(透過)"]');
 
+    await sendClick('button[name="職人モード(効果)"]');
+
+    if (params.get('size') != null) await sendClick('button[name="画像サイズ自動"]');
+    await setValue('div:has(button[name="画像サイズ自動"])+div>div>input.number', params.get('size') ?? 128);
+
     await sendClick('button[name="効果をつける(戻る)"]');
   }
 
   proc();
 
-  async function waitForElement(selector, callback) {
-    if (document.querySelector(selector)) {
-      callback(document.querySelector(selector));
-      return;
-    } else {
-      setTimeout(() => {
-        waitForElement(selector, callback);
-      }, 100);
-    }
+  async function waitForElement(selector) {
+    return new Promise(resolve => {
+      if (document.querySelector(selector)) {
+        resolve(document.querySelector(selector));
+      } else {
+        setTimeout(() => {
+          waitForElement(selector);
+        }, 100);
+      }
+    });
   }
 
   async function setValue(selector, value) {
     return new Promise(resolve => {
-      waitForElement(selector, (element) => {
+      waitForElement(selector).then(element => {
         element.value = value;
-        element.dispatchEvent(inputEvent);
-        element.dispatchEvent(changeEvent);
+        element.dispatchEvent(inputEvent(value));
+        element.dispatchEvent(changeEvent(value));
+        setTimeout(() => {
+          resolve();
+        }, 1000);
       });
-      resolve();
     });
   };
 
   async function sendClick(selector) {
     return new Promise(resolve => {
-      waitForElement(selector, (element) => {
+      waitForElement(selector).then(element => {
         element.click();
+        setTimeout(() => {
+          resolve();
+        }, 1000);
       });
-      resolve();
     });
   };
 });
